809a9383cd158c713eda4474972f136970ce74b2
MiguelOuellette2218 <miguel.ouellette@usherbrooke.ca>
2019-09-12 20:44:22 -0400

    Initialisation du projet

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+#include <LibRobus.h>
+
+#define PIN13 13
+
+void setup() {
+  // put your setup code here, to run once:
+
+  BoardInit();
+
+  pinMode(PIN13 , OUTPUT); 
+  Serial.begin(9600);
+
+  delay(1500);
+}
+
+void loop() {
+  if (ROBUS_IsBumper(2))
+  {
+    MOTOR_SetSpeed(0, -0.5);
+    MOTOR_SetSpeed(1, -0.5);
+    delay(4000);
+    MOTOR_SetSpeed(0, 0);
+    MOTOR_SetSpeed(1, 0);
+  }
+  if (ROBUS_IsBumper(3))
+  {
+    MOTOR_SetSpeed(0, 0.5);
+    MOTOR_SetSpeed(1, 0.5);
+    delay(4000);
+    MOTOR_SetSpeed(0, 0);
+    MOTOR_SetSpeed(1, 0);
+  }
+  if (ROBUS_IsBumper(0))
+  {
+    MOTOR_SetSpeed(1, 0.5);
+    delay(1000);
+    MOTOR_SetSpeed(1, 0);
+  }
+  if (ROBUS_IsBumper(1))
+  {
+    MOTOR_SetSpeed(0, 0.5);
+    delay(1000);
+    MOTOR_SetSpeed(0, 0);
+  }  
+}
